c1f99145bf15e7a0ee4d02873c0c4f19edcd0931
Ze Gan <zegan@microsoft.com>
2024-11-05 11:22:51 +0000

    ipfixrw
    Signed-off-by: Ze Gan <zegan@microsoft.com>

--- rust-ipfix/src/main.rs
+++ rust-ipfix/src/main.rs
@@ -1,6 +1,13 @@
+use ipfixrw::information_elements::get_default_formatter;
+use ipfixrw::parse_ipfix_message;
+use ipfixrw::parser::{DataRecord, DataRecordKey, DataRecordType, DataRecordValue};
+use ipfixrw::template_store::Template;
+use ahash::{HashMap, HashMapExt};
 use ipfix::{IpfixConsumer, IpfixPrinter};
-use std::fs::File;
-use std::io::prelude::*;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
 
 fn main() {
 
@@ -14,7 +21,7 @@ fn main() {
 
         0x01, 0x00, 0x00, 0x03, // line 5 Template ID 256, 3 fields
 
-        0x01, 0x45, 0x00, 0x04, // line 6 Field ID 325, 4 bytes
+        0x01, 0x45, 0x00, 0x08, // line 6 Field ID 325, 4 bytes
 
         0x80, 0x01, 0x00, 0x08, // line 7 Field ID 128, 8 bytes
         0x00, 0x01, 0x00, 0x01, // line 8 Enterprise Number 1, Field ID 1
@@ -25,67 +32,78 @@ fn main() {
     let template = Box::new(template_bytes);
 
     // contains data sets for templates 999, 500, 999
-    let data_bytes: [u8; 128] = [
-        0x00, 0x0A, 0x00, 0x40, // line 0
+    let data_bytes: [u8; 144] = [
+        0x00, 0x0A, 0x00, 0x48, // line 0
         0x00, 0x00, 0x00, 0x00, // line 1
         0x00, 0x00, 0x00, 0x02, // line 2
         0x00, 0x00, 0x00, 0x00, // line 3
 
-        0x01, 0x00, 0x00, 0x18, // line 4
+        0x01, 0x00, 0x00, 0x1C, // line 4
 
+        0x00, 0x00, 0x00, 0x00, // line 5
         0x00, 0x00, 0x00, 0x01, // line 5
         0x00, 0x00, 0x00, 0x00, // line 7
         0x00, 0x00, 0x00, 0x01, // line 8
         0x00, 0x00, 0x00, 0x00, // line 9
         0x00, 0x00, 0x00, 0x01, // line 10
 
-        0x01, 0x00, 0x00, 0x18, // line 11
+        0x01, 0x00, 0x00, 0x1C, // line 11
 
+        0x00, 0x00, 0x00, 0x00, // line 12
         0x00, 0x00, 0x00, 0x02, // line 12
         0x00, 0x00, 0x00, 0x00, // line 13
         0x00, 0x00, 0x00, 0x02, // line 14
         0x00, 0x00, 0x00, 0x00, // line 15
         0x00, 0x00, 0x00, 0x03, // line 16
 
-        0x00, 0x0A, 0x00, 0x40, // line 0
+        0x00, 0x0A, 0x00, 0x48, // line 0
         0x00, 0x00, 0x00, 0x00, // line 1
         0x00, 0x00, 0x00, 0x02, // line 2
         0x00, 0x00, 0x00, 0x00, // line 3
 
-        0x01, 0x00, 0x00, 0x18, // line 4
+        0x01, 0x00, 0x00, 0x1C, // line 4
 
+        0x00, 0x00, 0x00, 0x00, // line 5
         0x00, 0x00, 0x00, 0x01, // line 5
         0x00, 0x00, 0x00, 0x00, // line 7
         0x00, 0x00, 0x00, 0x01, // line 8
         0x00, 0x00, 0x00, 0x00, // line 9
         0x00, 0x00, 0x00, 0x04, // line 10
 
-        0x01, 0x00, 0x00, 0x18, // line 11
+        0x01, 0x00, 0x00, 0x1C, // line 11
 
+        0x00, 0x00, 0x00, 0x00, // line 12
         0x00, 0x00, 0x00, 0x02, // line 12
         0x00, 0x00, 0x00, 0x00, // line 13
         0x00, 0x00, 0x00, 0x02, // line 14
         0x00, 0x00, 0x00, 0x00, // line 15
         0x00, 0x00, 0x00, 0x07, // line 16
     ];
-    let data = Box::new(&data_bytes[64 .. ]);
+    // let data = Box::new(&data_bytes[0 .. ]);
 
-    let mut parser = IpfixConsumer::new();
+    let templates = Rc::new(RefCell::new(HashMap::new()));
+    let formatter = Rc::new(get_default_formatter());
+    let msg = parse_ipfix_message(&template_bytes, templates.clone(), formatter.clone()).unwrap();
+    println!("{:?}", msg);
 
-    let printer = IpfixPrinter::new();
 
-    assert!(parser.parse_message(&*template).is_ok());
-
-    if let Ok(datarecords) = parser.parse_message(&*data) {
-        let mut test_string = String::new();
-        for datarecord in datarecords {
-            let flows = printer.print_json(datarecord);
-            for flow in flows {
-                test_string += &flow;
+    let mut read_size: usize = 0;
+    while read_size < data_bytes.len() {
+        let mut len = ipfix::get_message_length(&data_bytes[read_size ..]).unwrap();
+        let data = &data_bytes[read_size .. read_size + len as usize];
+        let data_message = parse_ipfix_message(&data, templates.clone(), formatter.clone()).unwrap();
+        let datarecords: Vec<&DataRecord> = data_message.iter_data_records().collect();
+        for record in datarecords {
+            for (key, val) in record.values.iter() {
+                match key {
+                    DataRecordKey::Str(s) => println!("{}: {:?}", s, val),
+                    DataRecordKey::Unrecognized(k) => println!("Unrecognized: {:?} {:?}", k, val),
+                    _ => println!("Unrecognized"),
+                }
             }
         }
-
-        println!("{}", test_string);
+        read_size += len as usize;
     }
+
 
 }
